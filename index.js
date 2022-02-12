29f286d3b9ed18e676b5e76f4b92900c01b15e6c
Ilya <milyavskyfamily@gmail.com>
2022-02-11 23:50:20 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+
+function add(){
+    number++
+}
+
+function subtract(){
+    number--
+}
+
+function multiply(){
+    number *= number ;
+}
+
+function divide(){
+    number /= 5;
+}
+
+
+function add(a, b){
+    return (a + b)
+}
+
+function subtract(a, b){
+    return (a - b)
+}
+
+function multiply(a, b){
+    return (a * b)
+}
+
+function divide(a, b){
+    return (a / b)
+}
+
+
+function increment(a){
+return (a + 1) ;
+}
+
+function decrement(a){
+    return (a - 1) ;
+}
+
+function makeInt(a){
+    parseInt("a", 10) ;
+    return parseInt(a, 10);
+}
+
+function preserveDecimal(a){
+    parseFloat("a") ;
+    return parseFloat(a) ;
+}
